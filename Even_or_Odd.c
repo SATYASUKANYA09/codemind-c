0fcc32fdffd3988ace18fc5bd187dc08e9fd23a2
SATYASUKANYA09 <147488116+SATYASUKANYA09@users.noreply.github.com>
2023-10-16 15:36:40 +0530

    #include<stdio.h> int main() {     int m;     scanf("%d",&m);     if(m%2==0)     {         printf("Even");
        }
        else
        {
            printf("Odd");
        }
        
    }

---
+++ Even_or_Odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int m;
+    scanf("%d",&m);
+    if(m%2==0)
+    {
+        printf("Even");
+        
+    }
+    else
+    {
+        printf("Odd");
+    }
+    
+}
