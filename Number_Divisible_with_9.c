6f2c53e50d993057ad991df65da3cd362a6e15d6
SATYASUKANYA09 <147488116+SATYASUKANYA09@users.noreply.github.com>
2023-10-24 16:39:37 +0530

    #include<stdio.h> int main() {     int b;     scanf("%d",&b);     if(b%9==0)     {         printf("True");
        }
        else
        {
            printf("False");
        }
    }

---
+++ Number_Divisible_with_9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int b;
+    scanf("%d",&b);
+    if(b%9==0)
+    {
+        printf("True");
+        
+    }
+    else
+    {
+        printf("False");
+    }
+}
