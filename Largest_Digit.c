1a58dde7572be6821cb65c1f44d187c670dfa850
SATYASUKANYA09 <147488116+SATYASUKANYA09@users.noreply.github.com>
2023-12-04 18:23:20 +0530

    #include<stdio.h> int main() {     int n,remainder,largest=0;     scanf("%d",&n);     while(n>0)     {         remainder=n%10;         if(largest<remainder)         {             largest=remainder;         }         n=n/10;     }     printf("%d",largest); }

---
+++ Largest_Digit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,remainder,largest=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        remainder=n%10;
+        if(largest<remainder)
+        {
+            largest=remainder;
+        }
+        n=n/10;
+    }
+    printf("%d",largest);
+}
